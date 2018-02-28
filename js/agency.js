6e0747065b4faaeaead4dca39f448f60cbfcae02
James Massardo <james@dxrf.com>
2018-02-27 21:12:04 -0600

    inital commit

---
+++ js/agency.js
@@ -0,0 +1,75 @@
+-
+-
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+$('div.modal').on('show.bs.modal', function() {
+	var modal = this;
+	var hash = modal.id;
+	window.location.hash = hash;
+	window.onhashchange = function() {
+		if (!location.hash){
+			$(modal).modal('hide');
+		}
+	}
+});
+
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawGenderChart);
+google.charts.setOnLoadCallback(drawGradeChart);
+
+function drawGenderChart() {
+    var data = google.visualization.arrayToDataTable([
+    ['Gender', 'Count', { role: 'style' }, { role: 'annotation' }],
+    ['Female', 4, 'color: #E93F33', '4'],
+    ['Male', 15, 'color: #E93F33', '15']
+    ]);
+
+    var options = {
+    chartArea: {width: '65%'},
+    legend: { position: "none" }
+    };
+    var chart = new google.visualization.BarChart(document.getElementById('chart_gender'));
+    chart.draw(data, options);
+}
+
+function drawGradeChart() {
+    var data = google.visualization.arrayToDataTable([
+    ['Grade', 'Count', { role: 'style' }, { role: 'annotation' }],
+    ['Freshman', 2, 'color: #E93F33', '2'],
+    ['Sophomore', 10, 'color: #E93F33', '10'],
+    ['Junior', 7, 'color: #E93F33', '7'],
+    ['Senior', 0, 'color: #E93F33', '0']
+    ]);
+
+    var options = {
+    chartArea: {width: '65%'},
+    legend: { position: "none" }
+    };
+    var chart = new google.visualization.BarChart(document.getElementById('chart_grades'));
+    chart.draw(data, options);
+}
